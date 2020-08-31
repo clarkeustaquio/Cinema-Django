684723377d9c129e470ab823fa21bea3853a5f84
clark <clark.eustaquio@gmail.com>
2020-08-31 21:32:49 +0800

    Maintenance done for settings and mobile payment

--- payment/static/payment/script/stripe.js
+++ payment/static/payment/script/stripe.js
@@ -5,3 +5,11 @@ var checkOutButton = document.getElementById('checkout-button');
         }).then(function (result){
         });
     });
+
+var checkOutButtonMobile = document.getElementById('checkout-button-mobile');
+    checkOutButtonMobile.addEventListener('click', function(){
+        stripe.redirectToCheckout({
+            sessionId:  session
+        }).then(function (result){
+        });
+    });
